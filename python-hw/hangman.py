4157f354a10ab48f75b06ac31d5194760bf45fca
Denis Sorokin <denis.s.sorokin@gmail.com>
2017-07-21 00:35:24 -0500

    Added few conditionals to hangman.py

--- python-hw/hangman.py
+++ python-hw/hangman.py
@@ -8,11 +8,10 @@ money = 50
 player = True
 words = ['bird','ape','parrot','cat','kangaroo']
 
-while player == True or (attempts > 0 and money > 0):
+while player == True  or money > 0:
     word = random.choice(words)
     print(f'you have ${money} \nguesses left: {attempts}')
     print('_ ' * len(word))
-
     while attempts > 0 and money > 0:
         letter = input("Guess a letter.")
         if letter in word:
@@ -20,12 +19,26 @@ while player == True or (attempts > 0 and money > 0):
             print("You got it! \n \n \n" + "_" * 16)
             print('_ ' * len(word))
             print(f'you have ${money} \nguesses left: {attempts}')
+        elif letter in guesses:
+            print("You've already guessed this letter")
         else:
             money -= 5
             attempts -= 1
+            guesses.append(letter)
             print("You did not get it \n \n \n" + "_" * 16)
             print('_ ' * len(word))
             print(f'you have ${money} \nguesses left: {attempts}')
-            print(guesses.append(letter))
+            print(guesses)
+
+    play_again = input("would you like to play again? y/n")
+    if play_again == "y":
+        continue
+    elif play_again == "n":
+        break
+    else:
+        print("enter y or n")
+
+
+
 
 
